3847c119a7f7f10688b503f6819efc33e15576ce
Melvin Munoz <munoz.melvin@gmail.com>
2020-07-27 14:54:20 -0500

    se agrega cambios en archivo gitignore y se agrega nuevo archivo de spyder cia

---
+++ intelligence_agency/intelligence_agency/spiders/cia.py
@@ -0,0 +1,33 @@
+from scrapy import Spider
+
+class Cia(Spider):
+
+    name = 'cia'
+    start_urls = [
+        'https://www.cia.gov/library/readingroom/historical-collections'
+    ]
+    custom_settings = {  
+            "FEED_EXPORT" : 'utf-8',
+            "ROBOTSTXT_OBEY" : True,
+            "CONCURRENT_REQUEST" : 24,
+            "MEMUSAGE_LIMIT_MB" : 2048,
+            "MEMUSAGE_NOTIFY_MAIL" : ['munoz.melvin@gmail.com'],
+            "USER_AGENT" : "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.89 Safari/537.36"
+        }
+
+    def parse(self, response):
+        urls=response.xpath('//a[starts-with(@href,"collection") and (parent::h3 or parent::h2)]/@href').getall()
+
+        for link in urls :
+            
+            yield response.follow(link,callback=self.parse_link)
+
+    
+    def parse_link(self, response):
+        
+        title = response.xpath('//h1[@class="documentFirstHeading"]/text()').get()
+        body  = response.xpath('//div[@class="field-item even"]/p/text()').getall()
+
+        yield {'title' : title,
+                'body' : body
+            }
